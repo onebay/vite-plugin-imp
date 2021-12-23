207dcedf2a4ff72af0ee03aef1bebf444ce8edf2
pengsl <18718574603@163.com>
2021-12-24 00:34:13 +0800

    feat: add view-ui

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -6,6 +6,7 @@ import { ArcoDesignReactResolver } from './arco-design-react'
 import { ArcoDesignVueResolver } from './arco-design-vue'
 import { ElementPlusResolver } from './element-plus'
 import { ElementUIResolver } from './element-ui'
+import { ViewUIResolver } from './view-ui'
 import { LodashResolver } from './lodash'
 import { UnderScoreResolver } from './underscore'
 import { VantResolver } from './vant'
@@ -19,6 +20,7 @@ const defaultLibList: LibResolver[] = [
   ArcoDesignVueResolver,
   ElementPlusResolver,
   ElementUIResolver,
+  ViewUIResolver,
   LodashResolver,
   UnderScoreResolver,
   VantResolver,
---
+++ src/resolvers/view-ui.ts
@@ -0,0 +1,8 @@
+import { LibResolver } from '../types'
+
+export const ViewUIResolver: LibResolver = {
+  libName: 'view-design',
+  libDirectory: 'src/components',
+  replaceOldImport: true
+}
+
