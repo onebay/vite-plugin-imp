bb714a8ab49271b01343621380241118178d9606
psaren <18718574603@163.com>
2022-03-31 10:50:01 +0800

    fix: antd-mobile v5

--- src/resolvers/antd-mobile-v2.ts
+++ src/resolvers/antd-mobile-v2.ts
@@ -2,5 +2,5 @@ import { LibResolver } from '../types'
 
 export const AntdMobileReactResolver: LibResolver = {
   libName: 'antd-mobile',
-  style: (name) => `antd-mobile/es/components/${name}/${name}.css`
+  style: (name) => `antd-mobile/es/${name}/style/css.js`
 }
